652664622b1e68d3e039c0408ffaadb3fb62e721
Shreyas-Pattabi <spattabik@gmail.com>
2025-04-14 18:10:22 -0500

    new backend

--- routes/upload.py
+++ routes/upload.py
@@ -26,17 +26,17 @@ async def upload(file: UploadFile = File(...)):
         "url": public_url
     })
 
-@router.post("/recognize")
-async def upload_and_recognize(file: UploadFile = File(...)):
-    if not file.content_type.startswith("image/"):
-        raise HTTPException(status_code=400, detail="Invalid file type. Only images allowed.")
+# @router.post("/recognize")
+# async def upload_and_recognize(file: UploadFile = File(...)):
+#     if not file.content_type.startswith("image/"):
+#         raise HTTPException(status_code=400, detail="Invalid file type. Only images allowed.")
     
-    ext = os.path.splitext(file.filename)[1]
-    unique_filename = f"{uuid4().hex}{ext}"
+#     ext = os.path.splitext(file.filename)[1]
+#     unique_filename = f"{uuid4().hex}{ext}"
     
-    # Upload the image to S3
-    upload_to_s3(file, unique_filename)
+#     # Upload the image to S3
+#     upload_to_s3(file, unique_filename)
     
-    # Generate a pre-signed URL for temporary access
-    public_url = f"https://{S3_BUCKET_NAME}.s3.amazonaws.com/{unique_filename}"
-    pass
+#     # Generate a pre-signed URL for temporary access
+#     public_url = f"https://{S3_BUCKET_NAME}.s3.amazonaws.com/{unique_filename}"
+#     pass
